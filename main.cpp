7d7d94a44e2a3b85dbaf87ce3a6e2e90cae4d60f
Israel Mosqueda <isramestrada@gmail.com>
2026-04-17 20:11:24 -0700

    Created a struct called MediaItem to hold the other variables. Created an array of structs called MediaItem library. Also, created a variable to keep track of the number of items inside of the array.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,28 @@
 // CSCI-40 Final Project
-// Author: [Your Name Here]
+// Author: Israel Mosqueda
 
 /* 
  * Description:
- *
+ * Media backlog manager to organize games and movies.
 */
 
 #include <iostream>
+#include <string>
+using namespace std;
 
+struct MediaItem {  // Defined the struct
+    string title;
+    string category;
+    int year;
+    int rating;
+};
 int main() {
+
+    const int MAX_ITEMS = 100
+    MediaItem library[Max_ITEMS]; //declared the fixed-size array
+
+    int itemCount = 0; // to track how many items are in the array
+
+
     return 0;
 }
